e5e43840a76b1e3eeac2a70cfc8cf79c997edbca
HZYSDS <1292996846@qq.com>
2023-10-25 00:42:23 +1100

    the first

---
+++ src/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML private TextField usernameTF;
+    
+    @FXML private TextField passwordTF;
+
+    @FXML private Button loginButton;
+
+    @FXML private Button exitButton;
+}
